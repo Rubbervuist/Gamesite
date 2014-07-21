1f0da19a25ffaddf58aaa6031ec32eb85aaeb73a
Rubbervuist <r.c.vandenbovenkamp@students.uu.nl>
2014-07-21 18:22:29 +0100

    Create register.php

---
+++ register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Register page">
+    <meta name="keywords" content="Register page maffia">
+    <title>Register</title>
+    
+    <!--[if IE]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  	<st>
+ 	 <body style="text-align:center;font-family:verdana;">
+ 	 <h1>Register</h1>
+	 <form method="post" id="Register" action="registration.php">
+	 Username<br>
+	 <input type="text" name="username"> </input><br><br>
+ 	Email<br>
+	 <input type="text" id="Email_Register"> </input><br><br>
+	 Retype Email<br>
+	 <input type="text" id="ReEmail_Register"> </input><br><br>
+	 Password<br>
+	 <input type="text" name="password"> </input><br><br>
+	 Retype Password<br>
+	 <input type="text" name="confirmpass"> </input><br><br>
+	 <input type="radio" id="Radio_Male_Register" />Male  <input type="radio" id="Radio_Female_Register" />Female <br><br>
+ 	Date of Birth <br>
+	 <input type="date" id="Date_Of_Birth_Register"/><br><br>
+  	<input type="submit" value="Create My Account"></input>
+
+ </form>
+  </body>
+</html>
